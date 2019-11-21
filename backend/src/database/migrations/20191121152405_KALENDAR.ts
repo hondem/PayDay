f2d81272ccf42d60930ae9effbb4636859f2666e
tkorec <tomas.korec@greycortex.com>
2019-11-21 16:14:01 +0100

    [Backend] Add all migrations, delete .sql

---
+++ backend/src/database/migrations/20191121152405_KALENDAR.ts
@@ -0,0 +1,25 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+    knex.schema.raw(`
+    CREATE TABLE m.kalendar (
+        "typ" integer NOT NULL REFERENCES m.kalendar_typ(id),
+        "datum" date NOT NULL,
+        "pracovny_den" boolean NOT NULL,
+        "pracovne_hod" float NOT NULL,
+        "sviatok" boolean NOT NULL,
+        "sviatky_hod" float NOT NULL,
+        PRIMARY KEY (typ, datum)
+      );
+      COMMENT ON table m.kalendar_typ IS 'Riadky kalendara osahujuce jednotlive dni v roku s vyznacenim poctu pracovnych hodin a dni a poctu dni a hodin sviatkov';
+      COMMENT ON COLUMN m.kalendar.pracovne_dni IS ' je pracovny den (1 alebo 0)';
+      COMMENT ON COLUMN m.kalendar.pracovne_hod IS ' pocet pracovnych hodin pre dany den (0-24) (vacsinou 0-12)';
+      COMMENT ON COLUMN m.kalendar.sviatky_dni IS ' je sviatok (1 alebo 0)';
+      COMMENT ON COLUMN m.kalendar.sviatky_hod IS ' pocet hodin sviatku pre dany den (0-24) (vacsinou 0-8, pre 8 hodinovy pracovny cas 0 alebo 8)';`)
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+}
+
--- backend/src/database/sqldefs/m/kalendar_sviatky.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE m.kalendar_sviatky (
-  "datum" date PRIMARY KEY not null
-);
-COMMENT ON table m.kalendar_sviatky IS ' Obsahuje dni sviatkov';
-COMMENT ON COLUMN m.kalendar_datum IS ' datum sviatku (napr. 01/05/2019)';
-
