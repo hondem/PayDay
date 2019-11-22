bc9ca1ce4ab7aff07e2b62c16830d51ab5b21753
Jan Demel <jandemel98@gmail.com>
2019-11-22 15:17:55 +0100

    Added companies migration

---
+++ backend/src/database/migrations/20191122144525_COMPANIES.ts
@@ -0,0 +1,21 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+  // Create new table for companies
+  return knex.schema.createTable('companies', (table) => {
+    table.increments('id').primary()
+    table.string('name')
+  })
+  
+  // Create foreign key in users
+  .alterTable('users', (table) => {
+    table.integer('company').defaultTo(null)
+    table.foreign('company').references('companies.id')
+  })
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+}
+
