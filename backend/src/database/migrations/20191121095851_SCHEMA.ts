f26d6f62d8fa6b4c329656935dce6a84a9165739
tkorec <tomas.korec@greycortex.com>
2019-11-22 10:08:22 +0100

    [Backend] Add migration shema

---
+++ backend/src/database/migrations/20191121095851_SCHEMA.ts
@@ -0,0 +1,14 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+    return knex.schema.raw(`
+    CREATE SCHEMA m;
+    COMMENT ON SCHEMA m IS 'Mzdy';
+    `)
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+}
+
