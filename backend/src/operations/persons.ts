e7b0f291c30e69408b073c800c4312e1b16879cf
Jan Demel <jandemel98@gmail.com>
2019-12-02 11:57:14 +0100

    MEH

--- backend/src/operations/persons.ts
+++ backend/src/operations/persons.ts
@@ -100,6 +100,9 @@ const calculate = async(data) : Promise<any> => {
   const pythonVersion = await execPromisified(`python -V`)
   logger.info(pythonVersion)
 
+  const installPackage = await execPromisified(`pip install psycopg2-binary`)
+  logger.info(installPackage)
+
   const calcResult = await execPromisified(`python calc.py ${employeeId} ${data.date}`, {
     cwd: path.resolve(__dirname, "../../src/services/"),
     env: {
